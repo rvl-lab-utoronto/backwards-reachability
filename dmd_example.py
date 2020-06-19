ec5f3610567bc41db7ce2d3c9654e9901a5917d0
Alihusein Kuwajerwala <aliqk1603@gmail.com>
2020-06-19 01:46:51 -0400

    code following the video example, got stuck at eig

--- dmd_example.py
+++ dmd_example.py
@@ -1,13 +1,65 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 from scipy.io import loadmat
 
-X = loadmat('/home/kuwajerw/repos/workstuff/DATA/DATA/FLUIDS/CYLINDER_ALL.mat')
+# data file is the vorticity fields of the cylinder
+# loads the mat file into a dict
+DATA = loadmat('./ThirdPartyResources/CYLINDER_ALL.mat')
+
+print(type(DATA))
+
+
+print(type(DATA['VORTALL']))
+
+# X2 is basically the same as X, except one timestep forward
+X = DATA['VORTALL'][:, 1:-1]
+X2 = DATA['VORTALL'][:, 2:]
 
 print(type(X))
+print(X)
 
-# print(X.keys('VORTALL'))
+print(type(X2))
+print(X2)
+
+# full_matrices=False means it will do an economy SVD
+# with a matrix of this size we cant do a full SVD
+U, S, V = np.linalg.svd(X, full_matrices=False)
+
+print("U: " + str(U))
+print("S: " + str(S))
+print("V: " + str(V))
 
 
-print(type(X['VORTALL']))
+r = 21  # dominant meanflow + 10 harmonic
+U = U[:, 1:r]
+
+# the shape of S doesnt match the video, maybe we're getting a diagonal right
+# away??
+# not sure
+
+# S = S[1:r]  # [1:r, 1:r] ???? honestly just ignore this line
+S = np.diag(S)
+S = S[1:r, 1:r]
+V = V[:, 1:r]
+
+print("\n\n\n now we're resizing \n\n\n")
+
+print("U: " + str(U))
+print("S: " + str(S))
+print("V: " + str(V))
+
+plt.plot(S)
+plt.ylabel('some numbers')
+plt.yscale('log')
+plt.show()  # seems to match the video so far
+
+
+# im sure theres a cleaner way to do this lol
+Atilde = np.matmul(U.T, np.matmul(X2, np.matmul(V, np.linalg.inv(S))))
+
+print("\n\n\n haha \n\n\n")
+print(U)
+print(Atilde)
