1a652a3aea49fd978eaa7f3fcf6ac3b860e04ea7
Ali K <aliqk1603@gmail.com>
2020-07-11 18:18:59 -0400

    added basic control

--- take2/basic.py
+++ take2/basic.py
@@ -1,18 +1,25 @@
 import numpy as np
 from math import sin, cos, tan, pi
-import control
+# import control
 
 num_steps = 10
 
-A = np.array([[1., 0., -0.5],
-              [1., 1., -1.],
+def get_u_new(x, u_old, timestep):
+    control = 0.
+    if timestep >= 5:
+        control = pi/4
+    return np.array([0., 0., control])
+
+A = np.array([[1., -0.5, 0.],
+              [1., 1., 0.],
               [0., 0., 1.]])
 
-B = np.array([[1., 0., -0.5],
-              [1., 1., -1.],
+B = np.array([[0., 0., 0.],
+              [0., 0., 0.],
               [0., 0., 1.]])
 
-x_0 = np.array([1, 2, 3])
+x_0 = np.array([1., 2., 3.])
+u_0 = np.array([0., 0., 0.])
 
 path = []
 
@@ -20,11 +27,16 @@ path = []
 
 path.append(x_0)
 x_prev = x_0
+u_prev = u_0
 
 for i in range(num_steps):
-    x_new = A @ x_prev
+    print(A @ x_prev)
+    print(B @ u_prev)
+    x_new = (A @ x_prev) + (B @ u_prev)
     path.append(x_new)
     x_prev = x_new
+    u_prev = get_u_new(x_new, u_prev, i)
+
 
 for step in path:
     print(step)
