142cb289304a6a47aa2eaf49150e19450bcff5a4
Ali K <aliqk1603@gmail.com>
2020-07-12 23:48:11 -0400

    added optional control for reach

--- take2/reachability.py
+++ take2/reachability.py
@@ -2,9 +2,12 @@ import numpy as np
 from math import sin, cos, tan, pi
 from itertools import product
 
+EXCLUDE_CONTROL = True
+
 num_steps = 10
 # control_sp = [-1., 0., 1.]
 control_sp = [0., 1.]
+
 
 def get_u_new_p(x, u_old, timestep):
     possible_controls = control_sp
@@ -18,16 +21,20 @@ A = np.array([[1., 0., 1., 0.],
               [0., 0., 0., 0.],
               [0., 0., 0., 0.]])
 
-B = np.array([[0., 0., 0., 0.],
+B = np.array([[0., 0., 1., 0.],
+              [0., 0., 0., 1.],
               [0., 0., 0., 0.],
-              [0., 0., 1., 0.],
-              [0., 0., 0., 1.]])
+              [0., 0., 0., 0.]])
 
 x_0 = np.array([1., 2., 0., 0.])
 u_0 = np.array([0., 0., 1., 1.])
 
 reach = {}
-reach[str(x_0)] = "."
+if EXCLUDE_CONTROL:
+    reach[str(x_0[:2])] = "."
+else:
+    reach[str(x_0)] = "."
+
 queue =[]
 queue.append(x_0)
 
@@ -40,23 +47,32 @@ for i in range(num_steps):
     while queue:
         pn_state = queue.pop(0)
         # get all the possible things we could do
+        print("=============")
         print("curr op st: ", pn_state)
         u_outs = get_u_new_p(pn_state, u_prev, i)        
         # queue.remove(pn_state)
         for u in u_outs:
+            print("====")
             print("curr u: ", u)
             Ax = A @ pn_state
             Bu = B @ u
             pn_x_new = Ax + Bu
             print("Ax: ", Ax )
-            print("Bu:", Bu)
+            print("Bu: ", Bu)
             print("pn_x_new: ", pn_x_new)
-            if str(pn_x_new) not in reach:
+
+            if EXCLUDE_CONTROL:
+                check = str(pn_x_new[:2])
+            else:
+                check = str(pn_x_new)
+
+            if check not in reach:
                 print("new state!")
-                reach[str(pn_x_new)] = "."
+                reach[check] = "."
                 queue.append(pn_x_new)
+
         count += 1
-        if count > 10:
+        if count > 1000:
             break
         
 for idx, state in enumerate(reach):
