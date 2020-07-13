718863d52b9a697aa0f5570b74be14b103cca002
Ali Kuwajerwala <aliqk1603@gmail.com>
2020-07-13 19:19:45 -0400

    added backward dynamics

--- take2/backreach.py
+++ take2/backreach.py
@@ -2,11 +2,13 @@ import numpy as np
 from math import sin, cos, tan, pi
 from itertools import product
 
-EXCLUDE_CONTROL = True
+# EXCLUDE_CONTROL cannot be true if BACKWARD is true
+EXCLUDE_CONTROL = False
+BACKWARD = True
 
 num_steps = 10
-# control_sp = [-1., 0., 1.]
-control_sp = [0., 1.]
+control_sp = [-1., 0., 1.]
+# control_sp = [0., 1.]
 
 
 def get_u_new_p(x, u_old, timestep):
@@ -16,18 +18,31 @@ def get_u_new_p(x, u_old, timestep):
     final = [np.array([0., 0.,] + list(option)) for option in options]
     return final
 
+# I'm not sure if we need a 4x4 matrix here
+# At least for these simple dynamics, 
+# the two bottom rows are redundant as they don't affect anything 
 A = np.array([[1., 0., 1., 0.],
               [0., 1., 0., 1.],
-              [0., 0., 0., 0.],
-              [0., 0., 0., 0.]])
+              [0., 0., 1., 0.],
+              [0., 0., 0., 1.]])
 
-B = np.array([[0., 0., 1., 0.],
-              [0., 0., 0., 1.],
+bA = np.linalg.inv(A)
+
+B = np.array([[0., 0., 0., 0.],
               [0., 0., 0., 0.],
-              [0., 0., 0., 0.]])
+              [0., 0., 1., 0.],
+              [0., 0., 0., 1.]])
 
 x_0 = np.array([1., 2., 0., 0.])
 u_0 = np.array([0., 0., 1., 1.])
+
+print("Initial Conditions:")
+print("Dynamics:\n", A)
+print("Backward Dynamics:\n", bA)
+print("Control Matrix:\n", B)
+print("x_0: ", x_0)
+print("u_0: ", u_0, "\n\n")
+
 
 reach = {}
 if EXCLUDE_CONTROL:
@@ -53,11 +68,19 @@ for i in range(num_steps):
         # queue.remove(pn_state)
         for u in u_outs:
             print("====")
+            print("curr x:", pn_state)
             print("curr u: ", u)
-            Ax = A @ pn_state
             Bu = B @ u
-            pn_x_new = Ax + Bu
-            print("Ax: ", Ax )
+
+            if BACKWARD:
+                x__Bu = pn_state - Bu
+                pn_x_new = bA @ (x__Bu)
+                print("x__Bu: ", x__Bu)
+            else:
+                Ax = A @ pn_state
+                pn_x_new = Ax + Bu
+                print("Ax: ", Ax )
+            
             print("Bu: ", Bu)
             print("pn_x_new: ", pn_x_new)
 
