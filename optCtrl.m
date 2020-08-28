4a637973118944438ee1edcde01e11d890ed5aa1
Ali Kuwajerwala <aliqk1603@gmail.com>
2020-08-28 17:52:48 -0400

    example 3 code

---
+++ optCtrl.m
@@ -0,0 +1,24 @@
+function uOpt = optCtrl(obj, ~, ~, deriv, uMode)
+% uOpt = optCtrl(obj, t, y, deriv, uMode)
+
+%% Input processing
+if nargin < 5
+  uMode = 'min';
+end
+
+if ~iscell(deriv)
+  deriv = num2cell(deriv);
+end
+
+%% Optimal control
+if strcmp(uMode, 'max')
+  % uOpt = (deriv{obj.dims==3}>=0)*obj.wRange(2) + (deriv{obj.dims==3}<0)*(obj.wRange(1));
+  uOpt = (deriv{obj.dims==3}>=0)*obj.wRange(2) + (deriv{obj.dims==3}<0)*obj.wRange(2);
+elseif strcmp(uMode, 'min')
+  % uOpt = (deriv{obj.dims==3}>=0)*(obj.wRange(1)) + (deriv{obj.dims==3}<0)*obj.wRange(2);
+  uOpt = (deriv{obj.dims==3}>=0)*obj.wRange(2) + (deriv{obj.dims==3}<0)*obj.wRange(2);
+else
+  error('Unknown uMode!')
+end
+
+end
